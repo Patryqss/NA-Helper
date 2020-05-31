6eb108dccb50773bc6a6b6cc9979a0a67c310a21
Felipe <felipe_augusto_105@hotmail.com>
2020-05-31 20:40:40 -0300

    better UX and just-in-case restraints

--- frontend/src/components/DamageCalc/Health.jsx
+++ frontend/src/components/DamageCalc/Health.jsx
@@ -6,9 +6,10 @@ const Health = ({ handleChange, hp }) => (
       <span>HP </span>
     </div>
     <div className="body">
-      <input type="number" id="hp" onChange={handleChange} value={hp} max={100} />
+      <input type="number" id="hp" onChange={handleChange} value={hp.toString()} min={0} max={100} />
     </div>
   </div>
 );
 
 export default Health;
+
